ceb4c0fd28709ebe672ecde4da55c3fcb7d733c9
Tausif0209 <tk027592@gmail.com>
2025-08-22 22:04:20 +0530

    Leetcode 3195: Find the Minimum Area to Cover All Ones I

---
+++ MinimumAreasToCoverAllOnes.java
@@ -0,0 +1,16 @@
+public class MinimumAreasToCoverAllOnes {
+   public int minimumArea(int[][] grid) {
+        int minRow=grid.length,maxRow=-1,minCol=grid[0].length,maxCol=-1;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1) {
+                    minRow=Math.min(minRow,i);
+                    maxRow=Math.max(maxRow,i);
+                    minCol=Math.min(minCol,j);
+                    maxCol=Math.max(maxCol,j);
+                }
+            }
+        }
+        return (maxRow-minRow+1)*(maxCol-minCol+1);
+    }
+}
